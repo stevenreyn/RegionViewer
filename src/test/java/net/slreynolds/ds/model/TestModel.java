c031a9d66c136a593841d24ec26d3b749a21c61e
Steven Reynolds <steven.lamar.reynolds@gmail.com>
2013-09-13 22:06:34 -0500

    Added test of fields too

--- src/test/java/net/slreynolds/ds/model/TestModel.java
+++ src/test/java/net/slreynolds/ds/model/TestModel.java
@@ -35,6 +35,17 @@ public class TestModel {
 		}
 	}
 	
+	public static class UmClass {
+		private int one;
+		private double two;
+		private char three;
+		public UmClass(int one, double two, char three) {
+			this.one = one;
+			this.two = two;
+			this.three = three;
+		}
+	}
+	
 	@Test
 	public void testABCScenario() {
 		C c = new C(2);
@@ -60,5 +71,27 @@ public class TestModel {
 		GraphPoint gp_c = gp_b.getNeighbors().get(0).getTo();
 		assertEquals("c has no links",0,gp_c.getNeighbors().size());
 		
+	}
+	
+	@Test
+	public void testFields() {
+		UmClass um = new UmClass(1,2.0,'c');
+		ExporterStub exporter = new ExporterStub();
+		ObjectSaver saver = new ObjectSaver(exporter);
+		saver.save(new Object[]{um},new String[]{"um"},new HashMap<String,Object>());
+		Graph g = exporter.getGraph();
+		
+        assertEquals("num graph points",2,g.getGraphPoints().size());
+		
+		GraphPoint gp = g.getPrimaryGraphPoint();
+		assertTrue("primary point isa symbol",gp.hasAttr(Named.SYMBOL));
+		assertTrue("primary point isa symbol",(Boolean)gp.getAttr(Named.SYMBOL));
+		assertEquals("primary has one link",1,gp.getNeighbors().size());
+		
+		GraphPoint gp_um = gp.getNeighbors().get(0).getTo();
+		assertEquals("um has no links",0,gp_um.getNeighbors().size());
+		assertEquals("um.one",1,gp_um.getAttr("one"));
+		assertEquals("um.two",2.0,gp_um.getAttr("two"));
+		assertEquals("um.three",'c',gp_um.getAttr("three"));
 	}
 }
